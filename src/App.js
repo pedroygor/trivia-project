dc7930dea57d1f0d6a4e52a9814904c462f6d8f6
Vinicius Kaminski <vikaminski@hotmail.com>
2022-09-08 17:33:41 -0300

    Requisito 2 e 3 [Config,Game,Login,Token,Reduce Token, ServicesApi]

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import Login from './pages/Login';
 import Game from './pages/Game';
+import Config from './pages/Config';
 import Ranking from './pages/Ranking';
 import Feedback from './pages/Feedback';
 import './App.css';
@@ -14,6 +15,7 @@ export default function App() {
         <Route exact path="/game" component={ Game } />
         <Route exact path="/ranking" component={ Ranking } />
         <Route exact path="/feedback" component={ Feedback } />
+        <Route exact path="/config" component={ Config } />
       </Switch>
     </div>
   );

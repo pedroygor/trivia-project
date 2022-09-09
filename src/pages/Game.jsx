de3bee848b506e6795f9ad935992b550c528ff95
Vinicius Kaminski <vikaminski@hotmail.com>
2022-09-09 16:14:52 -0300

    mudanças

--- src/pages/Game.jsx
+++ src/pages/Game.jsx
@@ -1,8 +1,25 @@
 import React, { Component } from 'react';
+import { getAsks } from '../servicesApi';
 
 class Game extends Component {
+  async componentDidMount() {
+    const { history } = this.props;
+    const tokenVerify = JSON.parse(localStorage.getItem('token'));
+    const lengthVerify = 64;
+    if (tokenVerify.length !== lengthVerify) {
+      localStorage.setItem('token', '');
+      return history.push('/');
+    }
+    const returnResult = await getAsks(tokenVerify);
+    console.log(returnResult);
+  }
+
   render() {
-    return (<div>asdad</div>);
+    return (
+      <div>
+        asda
+      </div>
+    );
   }
 }
 
--- src/redux/actions/index.js
+++ src/redux/actions/index.js
@@ -1,7 +1,8 @@
-import { getTokenApi } from '../../servicesApi';
+import { getAsksApi, getTokenApi } from '../../servicesApi';
 
 export const ACTION_GENERIC = 'ACTION_GENERIC';
 export const TOKEN = 'TOKEN';
+export const SAVE_ASKS = 'SAVE_ASKS';
 
 export const requestActionGeneric = (payload) => ({
   type: ACTION_GENERIC,
@@ -13,9 +14,16 @@ export const tokenAction = (token) => ({
   token,
 });
 
+export const saveAsks = (asks) => ({
+  type: SAVE_ASKS,
+  asks,
+});
+
 export const fetchTokenAPI = () => (dispatch) => {
   getTokenApi().then((data) => {
-    localStorage.setItem('token', data.token);
+    localStorage.setItem('token', JSON.stringify(data.token));
     dispatch(tokenAction(data.token));
+    getAsksApi(data.token)
+      .then((questions) => dispatch(saveAsks(questions.results)));
   });
 };

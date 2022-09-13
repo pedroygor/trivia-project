9b5222e00b47a1b4d2845502c05a15b4ef00fc06
Diogo Rosa de Souza <diogo21souza@outlook.com.br>
2022-09-13 10:47:06 -0300

    Requisito 9

--- src/index.js
+++ src/index.js
@@ -2,13 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
+import { PersistGate } from 'redux-persist/integration/react';
 import App from './App';
-import store from './redux/store';
+import { store, persistor } from './redux/store';
 
 ReactDOM.render(
   <BrowserRouter>
     <Provider store={ store }>
-      <App />
+      <PersistGate loading={ null } persistor={ persistor }>
+        <App />
+      </PersistGate>
     </Provider>
   </BrowserRouter>,
   document.getElementById('root'),
--- src/pages/Ranking.jsx
+++ src/pages/Ranking.jsx
@@ -1,23 +1,68 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 
 class Ranking extends Component {
+  constructor() {
+    super();
+    this.state = {
+      players: [],
+    };
+  }
+
+  componentDidMount() {
+    const { players } = this.props;
+    const n = -1;
+    players.sort((a, b) => {
+      if (a.score > b.score) return n;
+      if (a.score < b.score) return 1;
+      return 0;
+    });
+    this.setState({ players });
+  }
+
   handleClick = () => {
     const { history } = this.props;
     history.push('/');
   };
 
   render() {
+    const { players } = this.state;
     return (
       <div>
-        <h1>Ranking</h1>
+        <h1 data-testid="ranking-title">Ranking</h1>
+        { players.map((player, index) => (
+          <div key={ player.hash }>
+            <img src={ `https://www.gravatar.com/avatar/${player.hash}` } alt="avatar" />
+            <div>
+              <p
+                data-testid={ `player-name-${index}` }
+              >
+                { player.name }
+              </p>
+              <div>
+                <span>
+                  Acertos:
+                  {' '}
+                  { player.assertions }
+                </span>
+                <span
+                  data-testid={ `player-score-${index}` }
+                >
+                  Pontuação:
+                  {' '}
+                  { player.score }
+                </span>
+              </div>
+            </div>
+          </div>
+        ))}
         <button
           type="button"
           data-testid="btn-go-home"
           onClick={ this.handleClick }
         >
           Home
-
         </button>
       </div>
     );
@@ -26,8 +71,13 @@ class Ranking extends Component {
 
 Ranking.propTypes = {
   history: PropTypes.shape({
-    push: PropTypes.func.isRequired,
-  }).isRequired,
-};
+    push: PropTypes.func,
+  }),
+  players: PropTypes.arrayOf,
+}.isRequired;
 
-export default Ranking;
+const mapStateToProps = (state) => ({
+  players: state.game.players,
+});
+
+export default connect(mapStateToProps)(Ranking);
